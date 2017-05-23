bbb6f9b2b60ecc1d4aeeead02a9658cf9f523551
Sojung Park <sojungko@gmail.com>
2017-05-23 15:21:00 -0400

    Remove console.log, add comments

--- src/state/actions.js
+++ src/state/actions.js
@@ -13,17 +13,17 @@ export function getPopularMovies () {
   // 3. each movie object in the results needs a releaseYear attribute added
   //    this is used in src/components/movies-list.js line 25
   //
-  const combinedResults = [...movies[0], ...movies[1]]
-    .map(movie => Object.assign({}, movie, { releaseYear: movie.releaseDate.substr(0, 4) }))
+
+  const combinedResults = [...movies[0], ...movies[1]] // Used spread operator to pull out movies from the two arrays
+    .map(movie => Object.assign({}, movie, { releaseYear: movie.releaseDate.substr(0, 4) })) // Added the releaseYear property to each movie object
     .sort((a, b) => {
-      if (a.releaseYear === b.releaseYear) {
-        console.log(a.title > b.title);
-        if (a.title > b.title) {
+      if (a.releaseYear === b.releaseYear) { // If the release year is the same
+        if (a.title > b.title) {             // Sort by title in ascending alphabetical order
           return 1;
         }
         return -1;
       }
-      return a.releaseYear - b.releaseYear;
+      return a.releaseYear - b.releaseYear;  // If the release year is not the same, sort movies by year in ascending order
     })
 
   return {
