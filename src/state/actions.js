2570aebd4b9d94abe594eb9d244cb32cf4bb74da
Sojung Park <sojungko@gmail.com>
2017-05-23 15:15:53 -0400

    Action populates sorted movies list

--- src/state/actions.js
+++ src/state/actions.js
@@ -13,8 +13,18 @@ export function getPopularMovies () {
   // 3. each movie object in the results needs a releaseYear attribute added
   //    this is used in src/components/movies-list.js line 25
   //
-
-  const combinedResults = []
+  const combinedResults = [...movies[0], ...movies[1]]
+    .map(movie => Object.assign({}, movie, { releaseYear: movie.releaseDate.substr(0, 4) }))
+    .sort((a, b) => {
+      if (a.releaseYear === b.releaseYear) {
+        console.log(a.title > b.title);
+        if (a.title > b.title) {
+          return 1;
+        }
+        return -1;
+      }
+      return a.releaseYear - b.releaseYear;
+    })
 
   return {
     type: 'GET_MOVIES_SUCCESS',
